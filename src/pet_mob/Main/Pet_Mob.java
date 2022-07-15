cd01b722486fdfac3e4daeb564b84f3ec47115c2
Thirbt <thiago.godo@hotmail.com>
2022-07-14 22:38:00 -0300

    trabalho final

---
+++ src/pet_mob/Main/Pet_Mob.java
@@ -0,0 +1,14 @@
+package pet_mob.Main;
+
+import pet_mob.View.TelaCarregamento;
+
+public class Pet_Mob {
+
+    public static void main(String[] args) {
+
+        TelaCarregamento telaCarregamento = new TelaCarregamento();;
+        telaCarregamento.setVisible(true);
+
+    }
+
+}
